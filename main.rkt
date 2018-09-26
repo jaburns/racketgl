55087a000e17197e89e81a16664b4b5d2bbaf9c0
Jeremy Burns <j@jaburns.net>
2018-09-26 17:58:22 -0600

    Adding SDL to C shell

--- main.rkt
+++ main.rkt
@@ -5,7 +5,14 @@
 
 (define-ffi-definer define-shell (ffi-lib "racketglshell"))
 
+#|
+(define-cstruct _ShellState ([sdl_window _pointer]
+                             [sdl_context _pointer]
+                             [window_width _int]
+                             [window_height _int]))
+(define-shell shell_init (_fun _string _int _int -> _ShellState))
+|#
+
 (define-cstruct _FloatPair ([first _float]
                             [second _float]))
-
 (define-shell add_floats (_fun _FloatPair -> _float))
